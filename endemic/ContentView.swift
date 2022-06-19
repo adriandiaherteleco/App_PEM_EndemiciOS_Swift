6e4b7431c870740917a9b4b38ecf2b15dbeac540
adriandiaherteleco <adrian>
2022-06-19 17:54:24 +0100

    Initial Commit

---
+++ endemic/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  endemic
+//
+//  Created by Adrián  Díaz Hernández on 19/6/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ endemic/endemicApp.swift
@@ -0,0 +1,17 @@
+//
+//  endemicApp.swift
+//  endemic
+//
+//  Created by Adrián  Díaz Hernández on 19/6/22.
+//
+
+import SwiftUI
+
+@main
+struct endemicApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
